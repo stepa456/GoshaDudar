6e70e11eb211f1c12b41e779652144e9724f13df
Usik182 <stepanusik262@gmail.com>
2024-04-19 00:17:34 +0500

    Изображения, кнопки, контейнеры

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,21 @@ class MyApp extends StatelessWidget {
           title: Text('Hello world!'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Text(
-            'Hello world!',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.red,
-              fontFamily: 'GoblinOne',
-            ),
-          ),
+        body: Container(
+          color: Colors.deepOrangeAccent,
+          child: Text('Hello world!'),
+          margin: EdgeInsets.fromLTRB(10.0, 15, 20.0, 30.0),
+          padding: EdgeInsets.all(20.5),
         ),
+        //Image(
+        //image: AssetImage('assets/child.jpg'),)
+        //image: NetworkImage('https://img.freepik.com/free-photo/the-adorable-illustration-of-kittens-playing-in-the-forest-generative-ai_260559-483.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1713312000&semt=ais'),
+
+        //TextButton.icon(label: Text('Нажми'),onPressed: () {},icon: Icon(Icons.adb_sharp),)
+        //RaisedButton.icon(onPressed: () {}, icon: Icon(Icons.adb_sharp), label: Text('Settins')),
+        //RaisedButton(onPressed: () {}, child: Text('Hello world')),
+        //FlatButton(onPressed: () {},child: Text('Нажми на кнопку!'),color: Colors.deepOrangeAccent),
+        //Icon(Icons.amp_stories_outlined, size: 45, color: Colors.amber),
         floatingActionButton: FloatingActionButton(
           child: Text('Нажми'),
           onPressed: () {
