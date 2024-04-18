6bb3c2c8473ea7cd8233a2659c28c214c75671e2
Usik182 <stepanusik262@gmail.com>
2024-04-18 23:34:34 +0500

    Основные виджет

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello world!'),
+          centerTitle: true,
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello world!',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.red,
+              fontFamily: 'GoblinOne',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Нажми'),
+          onPressed: () {
+            print('Нажми');
+          },
+          backgroundColor: Colors.deepOrangeAccent,
         ),
       ),
     );
